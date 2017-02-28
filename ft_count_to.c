f674a352ded1f25e6771052bbb879b5059ebf668
Anthony JUBERT <ajubert@e2r6p12.42.fr>
2017-02-28 14:09:21 +0100

    norme ft_count_to

--- ft_count_to.c
+++ ft_count_to.c
@@ -6,36 +6,17 @@
 /*   By: ajubert <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/23 15:48:44 by ajubert           #+#    #+#             */
-/*   Updated: 2017/02/25 13:57:04 by ajubert          ###   ########.fr       */
+/*   Updated: 2017/02/28 14:07:43 by ajubert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-int		ft_count_to(t_e *e, int i, char *str1)
+int		count_1(t_e *e, int i, char *str)
 {
 	int res;
 	int j;
-	char *str;
 
-	if (str1[ft_strlen(str1) - 1] == ',')
-	{
-		if (!(str = ft_strsub(str1, 2, ft_strlen(&str1[2]) - 1)))
-			exit(0);
-	}
-	else if (!(str = ft_strsub(str1, 2, ft_strlen(&str1[2]))))
-			exit(0);
-	res = 0;
-	j = -1;
-	while (i + ++j < e->nb_instruct)
-	{
-		res += e->tab[i + j].nb_octet;
-		if ((e->tab[i + j].cmd == 3 || e->tab[i+ j].cmd == 2) && ft_strcmp(str, e->tab[i + j].ft) == 0)
-		{
-			free_line(&str);
-			return (res);
-		}
-	}
 	res = 0;
 	j = -1;
 	while (i - ++j >= 0)
@@ -43,7 +24,8 @@ int		ft_count_to(t_e *e, int i, char *str1)
 		if (j == 0)
 			res += e->tab[i - j].nb_octet;
 		res -= e->tab[i - j].nb_octet;
-		if ((e->tab[i - j].cmd == 3 || e->tab[i- j].cmd == 2) && ft_strcmp(e->tab[i - j].ft, str) == 0)
+		if ((e->tab[i - j].cmd == 3 || e->tab[i - j].cmd == 2) &&
+				ft_strcmp(e->tab[i - j].ft, str) == 0)
 		{
 			free_line(&str);
 			return (res);
@@ -51,4 +33,33 @@ int		ft_count_to(t_e *e, int i, char *str1)
 	}
 	free_line(&str);
 	return (0);
+}
+
+int		ft_count_to(t_e *e, int i, char *str1)
+{
+	int		res;
+	int		j;
+	char	*str;
+
+	if (str1[ft_strlen(str1) - 1] == ',')
+	{
+		if (!(str = ft_strsub(str1, 2, ft_strlen(&str1[2]) - 1)))
+			exit(0);
+	}
+	else if (!(str = ft_strsub(str1, 2, ft_strlen(&str1[2]))))
+		exit(0);
+	res = 0;
+	j = -1;
+	while (i + ++j < e->nb_instruct)
+	{
+		res += e->tab[i + j].nb_octet;
+		if ((e->tab[i + j].cmd == 3 || e->tab[i + j].cmd == 2) &&
+				ft_strcmp(str, e->tab[i + j].ft) == 0)
+		{
+			free_line(&str);
+			return (res);
+		}
+	}
+	res = count_1(e, i, str);
+	return (res);
 }
